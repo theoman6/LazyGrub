369c662293a48bc2773df59ca34b20b2033b8727
theoman6 <tlevine@college.harvard.edu>
2013-05-29 21:08:50 -0400

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snackage::Application.config.secret_token = '6afacb23b4a0f01aa1538cc5e361e1940db339eca3338ea0c20c050243a330a9404054f1f5c7aedd86d3f3ebdd5718f0f6c64dc7e5c275aafbfebb9f5f57c947'
