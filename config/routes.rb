62f012a04dd27d342856fa454f90d14b62cf73e1
theoman6 <tlevine@college.harvard.edu>
2013-06-01 15:04:54 -0400

    added other side of home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Snackage::Application.routes.draw do
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
+  match '/deliveries', to: 'static_pages#deliveries'
 
   resources :orders, only: [:create, :destroy] 
 
