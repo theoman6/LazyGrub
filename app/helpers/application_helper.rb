a86970f0e05ac493085a438e00f5b78d74f35050
theoman6 <tlevine@college.harvard.edu>
2013-06-23 00:17:37 -0400

    made accordion on ajax load

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,17 +14,20 @@ module ApplicationHelper
      	end
 			"".tap do |i| 
 				simple_form_for(order) do |f|
+					i << "<div class = 'accordion'>"
 					item_list.each_key do |category| 
 						unless item_list[category].empty?
-							i << "<h4> #{category} </h4>"
+							i << " <h3> #{category} </h3><div class = 'tab'>"
               item_list[category].each do |item|
-                i << "<div class='control-group integer order_items_quantities> <div class = 'controls> <div class = 'row-fluid'> "
+                i << "<div class='control-group integer order_items_quantities> <div class = 'controls'> <div class = 'row-fluid'> "
                 i << "<div class ='span6'>" + item.name.to_s + ' $' + item.cost.to_s + "</div>"
                 i << '<div class ="span6"> <input type = "number" id = "item_' + item.id.to_s + '_quantity"' + 'class = "numeric integer item_quantities" name = "order[item_ids][' + item.id.to_s + ']" value = "0" data-cost = "' + item.cost.to_s + '">' 
-								i << "</input></div></div></div></div>"
+								i << "</input></div></div></div>"
 							end
+						i << '</div>'
 						end
 					end
+					i << '</div>'
 				end
 			end
 		end
