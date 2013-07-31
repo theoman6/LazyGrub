2e3ea649dc8330c8b9ec0a63e32fc461c98d128f
theoman6 <tlevine@college.harvard.edu>
2013-07-30 23:17:29 -0400

    better error handling on form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,27 +10,35 @@ class OrdersController < ApplicationController
   end
   
   def create
-    tmp = params['order']['item_ids']
-    choices = [] 
-    tmp.each_pair do |key, value| 
-      value.to_i.times do 
-        choices << {:item_id => key, :description => params['order']['item_descriptions'][key].shift[1]}
+    if user_signed_in?
+      tmp = params['order']['item_ids']
+      choices = [] 
+      tmp.each_pair do |key, value| 
+        value.to_i.times do 
+          choices << {:item_id => key, :description => params['order']['item_descriptions'][key].shift[1]}
+        end
       end
-    end
-    params['order'].delete('item_descriptions')
-    params['order'].delete('item_ids')
-    @order = current_user.orders.build(params['order'])
-    if @order.save
-      choices.each do |row| 
-        row[:order_id] = @order.id
-        choice = Choice.new(row)
-        choice.save
+      params['order'].delete('item_descriptions')
+      params['order'].delete('item_ids')
+      @order = current_user.orders.build(params['order'])
+      choices.map!{|choice| Choice.new(choice)}
+      if @order.save
+        choices.map! do |choice| 
+          choice[:order_id] = @order.id
+          choice.save
+          choice
+        end
+        flash[:success] = 'Order placed'
+        redirect_to orders_path
+      else 
+        flash[:alert] = "Order failed, try again"
+        binding.pry
+        @items = choices
+        render 'static_pages/home'
       end
-      flash[:success] = 'Order placed'
-      redirect_to orders_path
-    else 
-      flash[:alert] = "Order failed: see form for details"
-      render 'static_pages/home'
+    else
+      flash[:alert] = "You must be signed in to place orders"
+      redirect_to new_user_session_path
     end
   end
 
@@ -54,15 +62,20 @@ class OrdersController < ApplicationController
       end
       redirect_to orders_path
     else 
-      flash[:alert] = "Someone has already claimed this order. It can no longer be edited"
+      flash[:alert] = "Someone has already claimed this order. It can no longer be edited."
       redirect_to orders_path
     end
   end
 
 
   def destroy
-    Order.find(params[:id]).destroy
-    flash[:success] = 'Order Cancelled'
+    order = Order.find(params[:id])
+    if !order.claimer
+      order.destroy
+      flash[:success] = 'Order Cancelled'
+    else
+      flash[:alert] = 'Someone has already claimed this order. It can no longer be deleted.'
+    end
     redirect_to root_path
   end
 
